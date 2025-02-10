5d677a099ebbc01be84a11bad9282ad417a49ed6
holeto <114345147+holeto@users.noreply.github.com>
2025-02-10 16:46:53 +0100

    Added RNaD exploitability test on Jax Leduc

--- open_spiel/python/algorithms/mu_zero/experiments/utils.py
+++ open_spiel/python/algorithms/mu_zero/experiments/utils.py
@@ -1,4 +1,10 @@
 import pickle
+import pyspiel
+import jax
+import numpy as np
+
+from open_spiel.python.algorithms.mu_zero.jax_games.jax_game import JaxGame
+from open_spiel.python.policy import TabularPolicy
 
 def load_model(filepath: str):
   with open(filepath, "rb") as f:
@@ -8,3 +14,31 @@ def load_model(filepath: str):
 def save_model(filepath: str, data):
    with open(filepath, "wb") as f:
     pickle.dump(data, f)
+
+def stringify(a):
+  return ",".join(str(i) for i in a)
+
+#IMPORTANT!!! Will only work for games without chance nodes.
+def get_tabular_from_string(policy: dict, orig_game: pyspiel.Game, jax_game: JaxGame, tab_policy: TabularPolicy):
+  
+  def _traverse_tree(state, game_state, key, depth=0):
+    if state.is_terminal():
+      return
+    p1_iset = state.information_state_string(0) 
+    p2_iset = state.information_state_string(1)
+    _, jax_p1, jax_p2, jax_ps = jax_game.get_info(game_state)
+
+    for iset, jax_iset in zip([p1_iset, p2_iset], [jax_p1, jax_p2]):
+      pol = tab_policy.policy_for_key(iset)
+      for i in range(len(pol)):
+        pol[i] = policy[stringify(jax_iset)][i]
+    for a1 in state.legal_actions(0):
+      for a2 in state.legal_actions(1):
+        new_state = state.clone()
+        new_state.apply_actions([a1, a2])
+        new_game_state, new_key, new_terminal, new_rewards, new_legals = jax_game.apply_action(game_state, key, depth, np.array([a1, a2]))
+        _traverse_tree(new_state, new_game_state, new_key, depth +1)
+  #using seed 0 here, as this is not a game with chance nodes it can be arbitrary
+  start_key = jax.random.key(0)
+  _traverse_tree(orig_game.new_initial_state(), *jax_game.initialize_structures(start_key)[:-1])
+  return tab_policy    
