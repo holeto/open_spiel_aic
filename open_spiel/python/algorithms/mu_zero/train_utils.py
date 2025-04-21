2e3a5c2c2eb5e2779ad3b2c31d513d9a0c2c6ac3
kubicon3 <kubicon3@fel.cvut.cz>
2025-04-21 12:51:20 +0200

    Changed energy repulsion

--- open_spiel/python/algorithms/mu_zero/train_utils.py
+++ open_spiel/python/algorithms/mu_zero/train_utils.py
@@ -103,19 +103,24 @@ def _compute_soft_kmeans_loss_with_cluster_assignments(real:chex.Array, pred: ch
   
   cluster_difference = cluster_difference * valid[..., None, None]
   
-  cluster_distance = jnp.sum(cluster_difference **2, axis=-1)
-  cluster_distance = cluster_distance + (cluster_distance < 1e-15)
-  cluster_distance = cluster_distance ** 0.5
+  cluster_distance = jnp.sum(cluster_difference ** 2, axis=-1)
+  # cluster_distance = cluster_distance + (cluster_distance < 1e-15)
+  # cluster_distance = cluster_distance ** 0.5
+  
+  # cluster_loss = jax.nn.logsumexp(cluster_distance, axis=-1)
+  
   
   cluster_soft_assignement = compute_soft_assignments(cluster_distance, temperature, cluster_closeness_assignment, repulsive_force)
   
   # energy_repulsion = pred  
   cluster_each_other_distance = pred[..., :, None, :] - pred[..., None, :, :]
-  cluster_each_other_distance = jnp.mean(cluster_each_other_distance ** 2, axis=-1)
+  cluster_each_other_distance = jnp.sum(cluster_each_other_distance ** 2, axis=-1)
   
-  cluster_energy_repulsion = jnp.where(cluster_each_other_distance < 1e-8, 0, 1/(cluster_each_other_distance + 1e-8))
+  exp_energy_repulsion = jnp.exp(-cluster_each_other_distance)
   
-  cluster_energy_repulsion = jnp.mean(cluster_energy_repulsion) * 0.01
+  cluster_energy_repulsion = jnp.where(cluster_each_other_distance < 1e-8, 0, exp_energy_repulsion)
+  
+  cluster_energy_repulsion = jnp.mean(cluster_energy_repulsion) * 0.3
   
   cluster_loss = jnp.mean(cluster_difference ** 2, axis=-1)
   cluster_loss = jnp.sum(cluster_loss * cluster_soft_assignement, axis=-1) * valid
