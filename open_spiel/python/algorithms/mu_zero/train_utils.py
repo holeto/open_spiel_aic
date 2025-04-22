2595da89eacfe26991b6ae656d42220a9a408397
kubicon3 <kubicon3@fel.cvut.cz>
2025-04-22 18:16:06 +0200

    Changed the clustering again

--- open_spiel/python/algorithms/mu_zero/train_utils.py
+++ open_spiel/python/algorithms/mu_zero/train_utils.py
@@ -115,27 +115,17 @@ def compute_energy_repulsion_inverse(pred: chex.Array):
   cluster_energy_repulsion = jnp.mean(cluster_energy_repulsion)
   return cluster_energy_repulsion
 
+def compute_separation_loss(pred: chex.Array, cluster_closeness: float = 1.0):
+  cluster_each_other_distance = pred[..., :, None, :] - pred[..., None, :, :]
+  cluster_each_other_distance = jnp.sum(cluster_each_other_distance ** 2, axis=-1)
+  
+  separation_loss = jnp.maximum(0, cluster_closeness- cluster_each_other_distance)
+   
+  return jnp.mean(separation_loss)
 
-def _compute_soft_kmeans_sqrt_loss(real:chex.Array, pred: chex.Array, valid: chex.Array, temperature: float, cluster_closeness_assignment: float, repulsive_force: float):
-  chex.assert_shape((real,), (*pred.shape[:-2], pred.shape[-1]))
-  cluster_difference = lax.stop_gradient(jnp.expand_dims(real, -2)) - pred
-  
-  cluster_difference = cluster_difference * valid[..., None, None]
-  
-  cluster_distance = jnp.sum(cluster_difference ** 2, axis=-1)
-  cluster_distance = cluster_distance + (cluster_distance < 1e-15)
-  cluster_distance = cluster_distance ** 0.5
-  
-  cluster_soft_assignement = compute_soft_assignments(cluster_distance, temperature, cluster_closeness_assignment, repulsive_force)
-    
-
-    
-  cluster_energy_repulsion = compute_energy_repulsion_inverse(pred) * 0.01
-  
-  
-  cluster_loss = jnp.mean(cluster_difference ** 2, axis=-1)
-  cluster_loss = jnp.sum(cluster_loss * cluster_soft_assignement, axis=-1) * valid
-  return jnp.mean(cluster_loss) + cluster_energy_repulsion, cluster_soft_assignement
+def pullback_loss(pred: chex.Array):
+  return jnp.mean(jnp.sum(pred ** 2, axis=-1))
+ 
    
 
 # TODO: This should take valid into account
@@ -147,19 +137,20 @@ def _compute_soft_kmeans_loss_with_cluster_assignments(real:chex.Array, pred: ch
   cluster_difference = cluster_difference * valid[..., None, None]
   
   cluster_distance = jnp.sum(cluster_difference ** 2, axis=-1)
-  # cluster_distance = cluster_distance + (cluster_distance < 1e-15)
-  # cluster_distance = cluster_distance ** 0.5
+  cluster_distance = cluster_distance + (cluster_distance < 1e-15)
+  cluster_distance = cluster_distance ** 0.5
   
   # cluster_loss = jax.nn.logsumexp(cluster_distance, axis=-1)
   
   cluster_soft_assignement = compute_soft_assignments(cluster_distance, temperature, cluster_closeness_assignment, repulsive_force)
-    
-  cluster_energy_repulsion = compute_energy_repulsion(pred) * 0.3
-  
+
+  cluster_separation_loss = compute_separation_loss(pred, cluster_closeness_assignment) * 0.2
+  cluster_pullback_loss = pullback_loss(pred) * 0.001
   
   cluster_loss = jnp.mean(cluster_difference ** 2, axis=-1)
-  cluster_loss = jnp.sum(cluster_loss * cluster_soft_assignement, axis=-1) * valid
-  return jnp.mean(cluster_loss) + cluster_energy_repulsion, cluster_soft_assignement
+  cluster_loss = jnp.sum(cluster_loss * cluster_soft_assignement, axis=-1) * valid 
+  
+  return jnp.mean(cluster_loss) + cluster_separation_loss + cluster_pullback_loss, cluster_soft_assignement
   
 def _compute_soft_kmeans_loss_with_single(real: chex.Array, pred: chex.Array, probs: chex.Array, valid: chex.Array, temperature: float, cluster_closeness_assignment: float, repulsive_force: float):
   cluster_loss, cluster_soft_assignement = _compute_soft_kmeans_loss_with_cluster_assignments(real, pred, valid, temperature, cluster_closeness_assignment, repulsive_force)
